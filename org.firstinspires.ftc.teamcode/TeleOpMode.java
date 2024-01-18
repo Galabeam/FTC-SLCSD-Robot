9470a44e55dfa30dbaf6f0660640259ed9471b9d
Galabeam <galabeam76@gmail.com>
2024-01-18 09:57:44 -0500

    Comp 2 - v1.4 - organization

--- org.firstinspires.ftc.teamcode/TeleOpMode.java
+++ org.firstinspires.ftc.teamcode/TeleOpMode.java
@@ -43,16 +43,21 @@ public class TeleOpMode extends LinearOpMode {
       while (opModeIsActive()) {
         // Loop Code
         telemetry.update();
+        
         // Rear Wheels Forward/Backward
         LeftWheel.setPower(gamepad1.left_stick_y * -1);
         RightWheel.setPower(gamepad1.left_stick_y);
+
         // Rear Wheels Left/Right
         LeftWheel.setPower(gamepad1.left_stick_x);
         RightWheel.setPower(gamepad1.left_stick_x);
+
         // Sideways Wheel
         SidewaysWheel.setPower(gamepad1.right_stick_x);
+
         // Pixel Scooper
         PixelScooper.setPower(gamepad1.right_stick_y * -1);
+
         // Broom Spinner
         if (gamepad1.b) {
           BroomSpinner.setPower(0.3);
@@ -61,18 +66,21 @@ public class TeleOpMode extends LinearOpMode {
         } else if (BroomSpinner.getPower() != 0) {
           BroomSpinner.setPower(0);
         }
+
         // Conveyor Belt
         if (gamepad1.y) {
           ConveyorBelt.setPower(-0.75);
         } else if (ConveyorBelt.getPower() != 0) {
           ConveyorBelt.setPower(0);
         }
+
         // Airplane Launcher
         if (gamepad1.a) {
           AirplaneLauncher.setPosition(0.5);
         } else if (AirplaneLauncher.getPosition() != 0) {
           AirplaneLauncher.setPosition(0);
         }
+
         /* Wrong Buttons
         if (gamepad1.left_stick_button || gamepad1.right_stick_button || gamepad1.guide || gamepad1.start || gamepad1.back) {
           if (!SoundPlayer.CurrentlyPlaying) {
@@ -81,8 +89,6 @@ public class TeleOpMode extends LinearOpMode {
         }*/
       }
     }
-    
     androidSoundPool.close();
-    
   }
 }
